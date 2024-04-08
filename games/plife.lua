af0074f417cda70351c8b055e1b6e19a597da564
D <89603563+10dollarss@users.noreply.github.com>
2024-04-08 04:17:02 -0300

    Update plife.lua

--- games/plife.lua
+++ games/plife.lua
@@ -1,4 +1,5 @@
 local savepos = 0
+local cspawnpos = 0
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Cat Script - Prison Life", "Ocean")
 local Tab = Window:NewTab("Home")
@@ -8,9 +9,8 @@ Section:NewLabel("Prison Life")
 Section:NewLabel("GameID: 155615604")
 local Tab = Window:NewTab("Team")
 local Section = Tab:NewSection("  ")
-Section:NewButton("Criminal Team", "ButtonInfo", function(player)
+Section:NewButton("Criminal Team", "ButtonInfo", function()
     savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    print(savepos)
     local crimpart = Instance.new("Part")
     crimpart.Anchored = true
     crimpart.CanCollide = false
@@ -19,6 +19,16 @@ Section:NewButton("Criminal Team", "ButtonInfo", function(player)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = crimpart.CFrame
     wait(1.4)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
-    wait(2)
+    wait(1)
     crimpart:Destroy()
+end)
+Section:NewButton("Criminal Team 2", "ButtonInfo", function()
+    cspawnpos = game.workspace["Criminals Spawn"].SpawnLocation.CFrame
+    print(cspawnpos)
+    local cspawn = game.workspace["Criminals Spawn"].SpawnLocation
+    local humanoidrootpart = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    wait(0.8)
+    cspawn.CFrame = humanoidrootpart
+    wait(1)
+    cspawn.CFrame = cspawnpos
 end)
