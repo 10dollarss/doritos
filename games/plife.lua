4a775a3f597a96aa587edb3cb1f4e6633515ec13
D <89603563+10dollarss@users.noreply.github.com>
2024-04-08 03:57:00 -0300

    Update plife.lua

--- games/plife.lua
+++ games/plife.lua
@@ -1,13 +1,6 @@
-game.StarterGui:SetCore("SendNotification", {
-    Title    = "Prison Life"; 
-    Text     = "1.0"; 
-    Icon     = "http://www.roblox.com/asset?id=5205790785"; 
-    Duration = 7;
-    Callback = bindableFunction; 
-})
-
+local savepos = 0
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-local Window = Library.CreateLib("Cat Script - Prison Life", "DarkTheme")
+local Window = Library.CreateLib("Cat Script - Prison Life", "Ocean")
 local Tab = Window:NewTab("Home")
 local Section = Tab:NewSection("Home!!")
 Section:NewLabel("Cat Script's ")
@@ -15,14 +8,16 @@ Section:NewLabel("Prison Life")
 Section:NewLabel("GameID: 155615604")
 local Tab = Window:NewTab("Team")
 local Section = Tab:NewSection("  ")
-Section:NewButton("Criminal Team", "ButtonInfo", function()
-    local savepos = 0
-    wait(0.1)
-    savepos = game.workspace[Criminals Spawn].SpawnLocation.CFrame
-    game.workspace[Criminals Spawn].SpawnLocation.Anchored = false
-    game.workspace[Criminals Spawn].SpawnLocation.CFrame = game.players.localplayer.Character.HumanoidRootPart
+Section:NewButton("Criminal Team", "ButtonInfo", function(player)
+    savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    local crimpart = Instance.new("Part")
+    crimpart.Anchored = true
+    crimpart.CanCollide = false
+    crimpart.Transparency = 1
+    crimpart.CFrame = -920.510803, 92.2271957, 2138.27002, 0, 0 -1, 0, 1, 0, 1, 0, 0
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = crimpart.CFrame
+    wait(1.4)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
     wait(1)
-    savepos = game.workspace[Criminals Spawn].SpawnLocation.CFrame = savepos
-    wait(0.5)
-    savepos = 0
+    crimpart:Destroy()
 end)
