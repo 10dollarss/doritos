ce431eb6a9449eba265d9a80e46d0c1e41e95a18
D <89603563+10dollarss@users.noreply.github.com>
2024-04-08 05:53:15 -0300

    Update plife.lua

--- games/plife.lua
+++ games/plife.lua
@@ -1,6 +1,8 @@
-local savepos = 0
-local cspawnpos = 0
-local defaultspeed = 0
+-- local things
+local teamwait = false
+local savepos = 0 ---> Save Player Last Position
+
+-- main part
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Cat Script - Prison Life", "Ocean")
 local Tab = Window:NewTab("Home")
@@ -11,28 +13,20 @@ Section:NewLabel("GameID: 155615604")
 local Tab = Window:NewTab("Team")
 local Section = Tab:NewSection("  ")
 Section:NewButton("Criminal Team", "ButtonInfo", function()
+   if teamwait == false then
+    teamwait = true
     savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    local crimpart = Instance.new("Part")
-    crimpart.Anchored = true
-    crimpart.CanCollide = false
-    crimpart.Transparency = 1
-    crimpart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = crimpart.CFrame
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
     wait(1.4)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
-    wait(1)
-    crimpart:Destroy()
+    wait(0.5)
+    savepos = 0
+    teamwait = false
 end)
-Section:NewButton("Criminal Team 2", "ButtonInfo", function()
-    cspawnpos = game.workspace["Criminals Spawn"].SpawnLocation.CFrame
-    print(cspawnpos)
-    local cspawn = game.workspace["Criminals Spawn"].SpawnLocation
-    local humanoidrootpart = game.Players.LocalPlayer.Character.HumanoidRootPart
-    defaultspeed = humanoidrootpart.WalkSpeed
-    wait(0.8)
-    cspawn.CFrame = humanoidrootpart.CFrame
-    humanoidrootpart.WalkSpeed = 1
-    wait(1)
-    cspawn.CFrame = cspawnpos
-    humanoidrootpart.WalkSpeed = defaultspeed
+    
+Section:NewButton("Neutral Team", "ButtonInfo", function()
+    savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
+    wait(1.4)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
 end)
