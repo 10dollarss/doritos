1df1f5a2745e7bdf78d8f5ed0587a4c93bb85c08
D <89603563+10dollarss@users.noreply.github.com>
2024-04-08 04:05:16 -0300

    Update plife.lua

--- games/plife.lua
+++ games/plife.lua
@@ -8,16 +8,17 @@ Section:NewLabel("Prison Life")
 Section:NewLabel("GameID: 155615604")
 local Tab = Window:NewTab("Team")
 local Section = Tab:NewSection("  ")
-Section:NewButton("Criminal Team", "ButtonInfo", function()
+Section:NewButton("Criminal Team", "ButtonInfo", function(player)
     savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    print(savepos)
     local crimpart = Instance.new("Part")
     crimpart.Anchored = true
     crimpart.CanCollide = false
     crimpart.Transparency = 1
-    crimpart.CFrame = -920.510803, 92.2271957, 2138.27002, 0, 0 -1, 0, 1, 0, 1, 0, 0
+    crimpart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = crimpart.CFrame
     wait(1.4)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
-    wait(1)
+    wait(2)
     crimpart:Destroy()
 end)
