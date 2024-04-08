ed3c71fc44a2528b74f7b7bf096d4d2cdfffa595
D <89603563+10dollarss@users.noreply.github.com>
2024-04-08 03:39:55 -0300

    Update plife.lua

--- games/plife.lua
+++ games/plife.lua
@@ -1,2 +1,28 @@
--- i gonna put the code right here soon
-print("Hello World")
+game.StarterGui:SetCore("SendNotification", {
+    Title    = "Prison Life"; 
+    Text     = "1.0"; 
+    Icon     = "http://www.roblox.com/asset?id=5205790785"; 
+    Duration = 7;
+    Callback = bindableFunction; 
+})
+
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Window = Library.CreateLib("Cat Script - Prison Life", "DarkTheme")
+local Tab = Window:NewTab("Home")
+local Section = Tab:NewSection("Home!!")
+Section:NewLabel("Cat Script's ")
+Section:NewLabel("Prison Life")
+Section:NewLabel("GameID: 155615604")
+local Tab = Window:NewTab("Team")
+local Section = Tab:NewSection("  ")
+Section:NewButton("Criminal Team", "ButtonInfo", function(player)
+    local savepos = 0
+    wait(0.1)
+    savepos = game.workspace[Criminals Spawn].SpawnLocation.CFrame
+    game.workspace[Criminals Spawn].SpawnLocation.Anchored = false
+    game.workspace[Criminals Spawn].SpawnLocation.CFrame = player.Character.HumanoidRootPart
+    wait(1)
+    savepos = game.workspace[Criminals Spawn].SpawnLocation.CFrame = savepos
+    wait(0.5)
+    savepos = 0
+end)
