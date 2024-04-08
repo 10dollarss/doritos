930c6cbb403e430f9b32adb9caf266de874e27ba
D <89603563+10dollarss@users.noreply.github.com>
2024-04-08 05:54:51 -0300

    Update plife.lua

--- games/plife.lua
+++ games/plife.lua
@@ -13,20 +13,14 @@ Section:NewLabel("GameID: 155615604")
 local Tab = Window:NewTab("Team")
 local Section = Tab:NewSection("  ")
 Section:NewButton("Criminal Team", "ButtonInfo", function()
-   if teamwait == false then
-    teamwait = true
-    savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
-    wait(1.4)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
-    wait(0.5)
-    savepos = 0
-    teamwait = false
-end)
-    
-Section:NewButton("Neutral Team", "ButtonInfo", function()
-    savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
-    wait(1.4)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
-end)
+	if teamwait == false then
+		teamwait = true
+		savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
+		wait(1.4)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
+		wait(0.5)
+		savepos = 0
+		teamwait = false
+		end
+	end)
