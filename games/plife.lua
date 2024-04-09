c089824bc448207e61e3c311d39eb84b4de3d4a4
D <89603563+10dollarss@users.noreply.github.com>
2024-04-08 21:35:10 -0300

    Update plife.lua

--- games/plife.lua
+++ games/plife.lua
@@ -1,6 +1,18 @@
 -- local thingss
-local teamwait = false
 local savepos = 0 ---> Save Player Last Position
+
+-- team functions
+function Guards()
+        workspace.Remote.TeamEvent:FireServer("Bright blue")
+end
+function Criminals()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
+    wait(1.5)
+end
+function Inmate()
+    workspace.Remote.TeamEvent:FireServer("Bright Orange")
+end
+
 
 -- main part
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
@@ -13,14 +25,35 @@ Section:NewLabel("GameID: 155615604")
 local Tab = Window:NewTab("Team")
 local Section = Tab:NewSection("  ")
 Section:NewButton("Criminal Team", "ButtonInfo", function()
-	if teamwait == false then
-		teamwait = true
-		savepos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-976.125183, 109.123924, 2059.99536)
-		wait(1.4)
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
-		wait(0.5)
-		savepos = 0
-		teamwait = false
-		end
-	end)
+        local ab1 = false
+        if ab1 == false then
+            ab1 = true
+            Criminals()
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
+            wait(0.5)
+            savepos = 0
+            ab1 = false
+        end
+end)
+Section:NewButton("G Team", "ButtonInfo", function()
+    local ab2 = false
+    if ab2 == false then
+        ab2 = true
+        Guards()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
+        wait(0.5)
+        savepos = 0
+        ab2 = false
+    end
+end)
+Section:NewButton("I Team", "ButtonInfo", function()
+    local ab3 = false
+    if ab3 == false then
+        ab3 = true
+        Guards()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savepos
+        wait(0.5)
+        savepos = 0
+        ab3 = false
+    end
+end)
